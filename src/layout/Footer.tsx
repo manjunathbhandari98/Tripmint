2f0fbfdf992617a6b843fe873752de5965345d10
Manjunath <bhandarimanjunath98@gmail.com>
2026-03-06 21:51:45 +0530

    add footer link

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <footer className="w-full bg-white border-t border-gray-200 py-4 px-6 text-center">
       <p className="text-sm text-gray-600">
         © {new Date().getFullYear()} Tripmint · Developed by{" "}
-        <span className="font-semibold text-[#075E54]">Manjunath Bhandari</span>
+        <span className="font-semibold text-[#075E54]">
+          <Link to="https://manjunathbhandari.vercel.app/" target="_blank">
+            Manjunath Bhandari
+          </Link>
+        </span>
       </p>
     </footer>
   );
